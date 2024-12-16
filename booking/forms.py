01c1f55ebb8a59cbc77c7b4919509d297d940305
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-16 16:49:54 +0000

    add generic html templates to test functionality, bugfix booking forms to ensure Sunday cannot be submitted.

--- booking/forms.py
+++ booking/forms.py
@@ -11,6 +11,12 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['date', 'time_slot']
 
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if date.weekday() == 6:  # Sunday
+            raise forms.ValidationError("Bookings cannot be made on Sundays.")
+        return date
+
 
 class DisabledTimeSlotForm(forms.ModelForm):
     class Meta:
