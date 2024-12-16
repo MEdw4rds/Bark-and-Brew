c1e19619a8f600cafe36c3ab44a647383906bfea
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-16 16:45:50 +0000

    add BookingForm ,DisabledTimeSlotForm

---
+++ booking/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Booking, DisabledTimeSlot
+
+
+class BookingForm(forms.ModelForm):
+    date = forms.DateField( 
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
+
+    class Meta:
+        model = Booking
+        fields = ['date', 'time_slot']
+
+
+class DisabledTimeSlotForm(forms.ModelForm):
+    class Meta:
+        model = DisabledTimeSlot
+        fields = ['date', 'time_slot', 'reason']
