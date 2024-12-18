a28f122782eaf86690f8d0934de9d26fd8d8fd8f
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-18 08:43:28 +0000

    add CRUD views and bugfix for profile view

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import BookingForm
-from .models import Booking, DisabledTimeSlot
-from django.contrib.auth.decorators import login_required
 import datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .models import Booking, DisabledTimeSlot
+from .forms import BookingForm, EditBookingForm
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        booking_id = request.POST.get('booking_id')
+        if booking_id:
+            booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+            if booking.is_canceled:
+                booking.delete()
+                return redirect('profile')
+
+    active_bookings = Booking.objects.filter(user=request.user, is_canceled=False)
+    cancelled_bookings = Booking.objects.filter(user=request.user, is_canceled=True)
+    return render(request, 'booking/profile.html', {
+        'active_bookings': active_bookings,
+        'cancelled_bookings': cancelled_bookings
+    })
 
 
 @login_required
@@ -14,11 +34,9 @@ def book_time_slot(request):
             booking.user = request.user
             booking.save()
             return redirect('booking_success')
-        else:
-            print("Form is not valid:", form.errors)
     else:
         form = BookingForm()
-
+    
     selected_date = request.GET.get('date', None)
     if selected_date:
         selected_date = datetime.datetime.strptime(selected_date, '%Y-%m-%d').date()
@@ -33,3 +51,32 @@ def book_time_slot(request):
         'taken_time_slots': taken_time_slots,
         'disabled_time_slots': disabled_time_slots
     })
+
+
+@login_required
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+    if request.method == 'POST':
+        if 'cancel' in request.POST:
+            booking.is_canceled = True
+            booking.save()
+            return redirect('profile')
+        elif 'delete' in request.POST:
+            booking.delete()
+            return redirect('profile')
+        else:
+            form = EditBookingForm(request.POST, instance=booking)
+            if form.is_valid():
+                form.save()
+                return redirect('profile')
+    else:
+        form = EditBookingForm(instance=booking)
+    return render(request, 'booking/edit_booking.html', {
+        'form': form,
+        'booking': booking
+    })
+
+
+def booking_success(request):
+    return render(request, 'booking/booking_success.html')
+
