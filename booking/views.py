af386d5ee8e0984c274176bd299d2718db17d4e2
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-19 10:07:02 +0000

    edit to pass python linter line character limit

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -7,18 +7,25 @@ from .models import Booking, DisabledTimeSlot
 from .forms import BookingForm, EditBookingForm
 
 
-@login_required
 def profile(request):
     if request.method == 'POST':
         booking_id = request.POST.get('booking_id')
         if booking_id:
-            booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+            booking = get_object_or_404(
+                Booking,
+                id=booking_id,
+                user=request.user
+                )
             if booking.is_canceled:
                 booking.delete()
                 return redirect('profile')
 
-    active_bookings = Booking.objects.filter(user=request.user, is_canceled=False)
-    cancelled_bookings = Booking.objects.filter(user=request.user, is_canceled=True)
+    active_bookings = Booking.objects.filter(
+        user=request.user, is_canceled=False
+        )
+    cancelled_bookings = Booking.objects.filter(
+        user=request.user, is_canceled=True
+        )
     return render(request, 'booking/profile.html', {
         'active_bookings': active_bookings,
         'cancelled_bookings': cancelled_bookings
@@ -34,14 +41,20 @@ def book_time_slot(request):
             booking.user = request.user
             booking.save()
             return redirect('booking_success')
-    else:
+    else:  # This is for the GET request
         form = BookingForm()
-    
+
     selected_date = request.GET.get('date', None)
     if selected_date:
-        selected_date = datetime.datetime.strptime(selected_date, '%Y-%m-%d').date()
-        taken_time_slots = Booking.objects.filter(date=selected_date, is_canceled=False).values_list('time_slot', flat=True)
-        disabled_time_slots = DisabledTimeSlot.objects.filter(date=selected_date).values_list('time_slot', flat=True)
+        selected_date = datetime.strptime(selected_date, '%Y-%m-%d').date()
+        queryset = Booking.objects.filter(
+            date=selected_date, is_canceled=False
+            )
+        taken_time_slots = queryset.values_list('time_slot', flat=True)
+        querysetTwo = DisabledTimeSlot.objects.filter(
+            date=selected_date
+            )
+        disabled_time_slots = querysetTwo.values_list('time_slot', flat=True)
     else:
         taken_time_slots = []
         disabled_time_slots = []
@@ -77,6 +90,6 @@ def edit_booking(request, booking_id):
     })
 
 
+@login_required
 def booking_success(request):
     return render(request, 'booking/booking_success.html')
-
