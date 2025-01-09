c99146c30a3a6f74ebcb77f1d8d4c0bdd6ce14fd
Matthew Edwards <wste993l@students.codeinstitute.net>
2025-01-09 08:50:10 +0000

    replace jpg images with webp and update all image links, bugfix booking admin to show relevent data on admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Booking, DisabledTimeSlot
 
 # Register your models here.
-admin.site.register(Booking)
-admin.site.register(DisabledTimeSlot)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Displays list of current bookings in the admin panel.
+    allows searching by user and date.
+    """
+
+    list_display = ('user', 'date', 'is_canceled')
+
+
+@admin.register(DisabledTimeSlot)
+class DisabledTimeSlotAdmin(admin.ModelAdmin):
+    """
+    Displays list of current timeslots disabled by the admin in the admin
+    panel. Allows searching by Date, timeslot and Reason.
+    """
+
+    list_display = ('date', 'time_slot', 'reason')
